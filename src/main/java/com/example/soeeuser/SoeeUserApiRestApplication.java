3994a5d4eea36bdfac67034affa25847b4a1a634
nayibimartin <nmartin@onemarketer.net>
2022-09-20 15:37:20 -0400

    Initial Commit

---
+++ src/main/java/com/example/soeeuser/SoeeUserApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.soeeuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoeeUserApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoeeUserApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soeeuser/SoeeUserApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soeeuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoeeUserApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
